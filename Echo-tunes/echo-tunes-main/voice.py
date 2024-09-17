23e9332eb75efecc2e8f0abb7c7b98e3264b34d9
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-18 00:11:11 +0530

    create new file voice.py

---
+++ Echo-tunes/echo-tunes-main/voice.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+
+# Initialize recognizer
+r = sr.Recognizer()
+
+
+# Function to recognize speech
+def recognize_speech():
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print(text)
+            return text.lower()
+        except:
+            return ""
+
+
+def parse_voice_command() -> str:
+    com = recognize_speech()
+    if com == "":
+        return ""
+    else:
+        return com
